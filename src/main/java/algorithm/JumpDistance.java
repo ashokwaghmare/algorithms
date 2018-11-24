9002b2624ca95848c0e2b39b9e6376a438726234
ashokwaghmare <ashok.waghmare.cse@gmail.com>
2018-11-24 23:22:01 +0530

    jump right solution;

--- src/main/java/algorithm/JumpDistance.java
+++ src/main/java/algorithm/JumpDistance.java
@@ -2,17 +2,14 @@ package algorithm;
 
 public class JumpDistance {
 
-    public int jump(int x, int y, int D) {
-
+    public int jumpOne(int x, int y, int D) {
         if(x == y) return 0;
-
-        int Distance = x;
-        int jump = D;
-        int jumpCount = 0;
-        do{
-           Distance = Distance + jump;
-           jumpCount = jumpCount + 1;
-        }while(Distance < y);
-        return jumpCount;
+        int toBeCovered = y - x;
+        int jump = toBeCovered / D;
+        if(((jump*D)+x) >= y){
+            return jump;
+        } else {
+            return jump +1;
+        }
     }
 }
--- src/test/java/algorithm/JumpDistanceTest.java
+++ src/test/java/algorithm/JumpDistanceTest.java
@@ -8,42 +8,42 @@ public class JumpDistanceTest {
 
     @Test
     public void jumpOne() throws Exception {
-        int output = new JumpDistance().jump(10, 85, 30);
+        int output = new JumpDistance().jumpOne(10, 85, 30);
 
         assertEquals(output, 3);
     }
 
     @Test
     public void jumpTwo() throws Exception {
-        int output = new JumpDistance().jump(100, 100, 30);
+        int output = new JumpDistance().jumpOne(100, 100, 30);
 
         assertEquals(output, 0);
     }
 
     @Test
     public void jumpTwo1() throws Exception {
-        int output = new JumpDistance().jump(100, 110, 30);
+        int output = new JumpDistance().jumpOne(100, 110, 30);
 
         assertEquals(output, 1);
     }
 
     @Test
     public void jumpTwo2() throws Exception {
-        int output = new JumpDistance().jump(100, 130, 30);
+        int output = new JumpDistance().jumpOne(100, 130, 30);
 
         assertEquals(output, 1);
     }
 
     @Test
     public void jumpTwo3() throws Exception {
-        int output = new JumpDistance().jump(100, 140, 30);
+        int output = new JumpDistance().jumpOne(100, 140, 30);
 
         assertEquals(output, 2);
     }
 
     @Test
     public void jumpTwo4() throws Exception {
-        int output = new JumpDistance().jump(1, 140, 30);
+        int output = new JumpDistance().jumpOne(1, 140, 30);
 
         assertEquals(output, 5);
     }
