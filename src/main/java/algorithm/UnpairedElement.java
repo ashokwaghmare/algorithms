750578c71600d340ccb7504b8db309f0791ac889
ashokwaghmare <ashok.waghmare.cse@gmail.com>
2018-11-25 17:18:23 +0530

    unpaired elements correct one

--- src/main/java/algorithm/UnpairedElement.java
+++ src/main/java/algorithm/UnpairedElement.java
@@ -1,29 +1,21 @@
 package algorithm;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class UnpairedElement {
 
     public int unpairedElement(int[] arr) {
-        List<Integer> list = arrayToList(arr);
-        List<Integer> sorted = new Sort().quickSort(list);
-        int unpaired = 0;
-        for(int i=0; i<sorted.size()-1; i++){
-            if(sorted.get(i) != sorted.get(i+1)){
-                unpaired = sorted.get(i);
-            }
+        int Unpaired = arr[0];
+        for(int i =1; i < arr.length; i++) {
+            Unpaired = Unpaired ^ arr[i];
         }
-        return unpaired;
+        return Unpaired;
     }
 
-    private List<Integer> arrayToList(int[] arr) {
-        return Arrays.stream(arr).boxed().collect(Collectors.toList());
-    }
 
-    private int[] listToArray(List<Integer> list) {
-        return list.stream().mapToInt(i -> i).toArray();
-    }
 }
